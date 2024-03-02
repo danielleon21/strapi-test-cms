b5f006e5b91b9207c4db46858d939c95a03359dc
Daniel Leon <carlos.matus@coppel.com>
2024-03-01 23:34:39 -0600

    first commit

---
+++ src/api/shelter/controllers/shelter.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * shelter controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::shelter.shelter');
---
+++ src/api/shelter/routes/shelter.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * shelter router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::shelter.shelter');
